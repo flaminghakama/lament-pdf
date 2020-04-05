06520fd9af4adfa16f2f627881114674d5c18dc7
David Elaine Alt <elaine@flaminghakama.com>
2020-04-04 21:35:52 -0700

    initial attempt at manifest

--- manifest.js
+++ manifest.js
@@ -8,19 +8,19 @@ altJazzSongs["Lament"] = {
 
     partsInBooks: {
         "Flute, Alto Flute & Piccolo": {
-            "Lament": [ "Flute" ], 
+            //"Lament": [ "Flute" ], 
         },
         "English Horn": {
-            "Lament": [ "English Horn in F" ], 
+            //"Lament": [ "English Horn in F" ], 
         },
         "Bass Clarinet": {
-            "Lament": [ "Bass Clarinet in Bb" ],
+            //"Lament": [ "Bass Clarinet in Bb" ],
         },
         "Voice/Percussion": {
-            "Lament": [ "Voice" ],
+            //"Lament": [ "Voice" ],
         },
         "Drums": {
-            "Lament": [ "Drums" ], 
+            //"Lament": [ "Drums" ], 
         },
         "Violin I": {
             "Lament": [ "Violin I" ], 
@@ -46,41 +46,43 @@ altJazzSongs["Lament"] = {
                 title: "Lament",
                 composer: "Elaine Paul",
                 arranger: "Elaine Paul",
-                genre: "Afro-Cuban Jazz",
-                bpm: 'BPM',
-                key: "KEY"
+                genre: "Classical",
+                bpm: '60bpm',
+                key: "B Minor"
             },
-            fileLocation: "scores/lament/pdf",
-            filePrefix: "lament",
+            baseUrl: 'http://ali-cia.net/altjazz/scores/', 
+            pdfSubdir: "/",
+            fileLocation: "lament",
+            filePrefix: "lament-",
             parts: {
-                "Flute": { fileSuffix: "Flute" },
-                "English Horn in F": { fileSuffix: "English-Horn-in-F" },
-                "Clarinet in A": { fileSuffix: "Clarinet-in-A" },
-                "Bass Clarinet in Bb": { fileSuffix: "Bass-Clarinet-in-Bb" },
-                "Bassoon": { fileSuffix: "Bassoon" },
-                "Trumpet in Bb": { fileSuffix: "Trumpet-in-Bb" },
-                "Horn in F": { fileSuffix: "Horn-in-F" },
-                "Trombone": { fileSuffix: "Trombone" },
-                "Clave": { fileSuffix: "Clave" },
-                "Piano": { fileSuffix: "Piano" },
-                "Voice": { fileSuffix: "Voice" },
+                //"Flute": { fileSuffix: "Flute" },
+                //"English Horn in F": { fileSuffix: "English-Horn-in-F" },
+                //"Clarinet in A": { fileSuffix: "Clarinet-in-A" },
+                //"Bass Clarinet in Bb": { fileSuffix: "Bass-Clarinet-in-Bb" },
+                //"Bassoon": { fileSuffix: "Bassoon" },
+                //"Trumpet in Bb": { fileSuffix: "Trumpet-in-Bb" },
+                //"Horn in F": { fileSuffix: "Horn-in-F" },
+                //"Trombone": { fileSuffix: "Trombone" },
+                //"Clave": { fileSuffix: "Clave" },
+                //"Piano": { fileSuffix: "Piano" },
+                //"Voice": { fileSuffix: "Voice" },
                 "Violin I": { fileSuffix: "Violin-I" },
                 "Violin II": { fileSuffix: "Violin-II" },
                 "Viola": { fileSuffix: "Viola" },
                 "Cello": { fileSuffix: "Cello" },
-                "Bass": { fileSuffix: "Bass" },
+                //"Bass": { fileSuffix: "Bass" },
             },
             scores: {
-                "Rhythm": { fileSuffix: "Rhythm" },
-                "Condensed Score": { fileSuffix: "Condensed-Score" },
+                //"Rhythm": { fileSuffix: "Rhythm" },
+                //"Condensed Score": { fileSuffix: "Condensed-Score" },
                 "Score Transposed": { fileSuffix: "Score-Transposed" },
-                "Score Concert": { fileSuffix: "Score-Concert" },
-                "Lead Sheet": { fileSuffix: "Lead-Sheet" },
+                //"Score Concert": { fileSuffix: "Score-Concert" },
+                //"Lead Sheet": { fileSuffix: "Lead-Sheet" },
             },
             recordings: {
                 demo: {
                     description: 'demo',
-                    url: ''
+                    url: 'https://soundcloud.com/david-elaine-alt/lament-midi'
                 },
                 reading: {
                     description: 'reading',
